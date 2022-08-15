41f34d4c8b772e93ea3ea6e287a0c726765ddc2d
OleksandrLevinskyi <aleksandrlevinskyi@gmail.com>
2022-08-15 03:48:22 -0400

    interact with open weather api

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,8 @@
 import 'package:clima/services/location.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -23,5 +26,30 @@ class _LoadingScreenState extends State<LoadingScreen> {
     await location.getCurrentLocation();
     print(location.latitude);
     print(location.longitude);
+
+    await getData();
+  }
+
+  Future<void> getData() async {
+    Uri url = Uri.https(
+      dotenv.env['OPEN_WEATHER_API_URL'],
+      dotenv.env['OPEN_WEATHER_API_PATH'],
+      {
+        'lat': '50',
+        'lon': '50',
+        'appid': dotenv.env['OPEN_WEATHER_API_KEY'],
+      },
+    );
+
+    var response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      var jsonResponse =
+          convert.jsonDecode(response.body) as Map<String, dynamic>;
+
+      print(jsonResponse);
+    } else {
+      print('Request failed with status: ${response.statusCode}.');
+    }
   }
 }
